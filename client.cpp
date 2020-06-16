c2eb49e7dd0690b2aee7c32149fcc7e691fd1bf9
KechunFish <1105279001@qq.com>
2020-06-16 21:50:12 +0800

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#define PORT 8080 
+
+int main(int argc, char const *argv[])
+{
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "212.64.33.194", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    while(1)
+    {
+        char message[1024];
+        scanf("%s", message);
+        send(sock , message , strlen(message) , 0 );        
+    }
+    return 0;
+}
